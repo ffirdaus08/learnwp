7678ec3470eea8d79b6bf3b7a87251a684621869
unknown <you@example.com>
2020-08-31 00:00:09 +0700

    30082020 add function title

--- wp-content/themes/jifl/functions.php
+++ wp-content/themes/jifl/functions.php
@@ -8,3 +8,9 @@ function jifl_files(){
 }
 
 add_action('wp_enqueue_scripts','jifl_files');
+
+function university_features(){
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'university_features');
