d3986db19a22076fd7c8d383fe7133178451d82e
fahmi <fahmifirdaus07@gmail.com>
2020-12-03 20:52:03 +0700

    +T25Install NodeJs

--- wp-content/themes/jifl/functions.php
+++ wp-content/themes/jifl/functions.php
@@ -1,10 +1,9 @@
 <?php
 
 function jifl_files(){
-    wp_enqueue_script('jifl_main_script', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
     wp_enqueue_style('font-google','https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome','https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('jifl_main_styles', get_stylesheet_uri());
+    wp_enqueue_script('jifl_main_script', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
 }
 
 add_action('wp_enqueue_scripts','jifl_files');
